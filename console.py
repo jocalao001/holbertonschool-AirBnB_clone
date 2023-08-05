d6aa16311bab019e4b21bb952ecc6bc8af50e784
jocalao001 <113933963+jocalao001@users.noreply.github.com>
2023-08-05 00:05:08 -0500

    update

---
+++ console.py
@@ -0,0 +1,133 @@
+#!/usr/bin/python3
+# console.py
+# Jose Tirado <5698@holbertonstudents.com>
+"""Define a console implementation."""
+
+
+import cmd
+from models.base_model import BaseModel
+from models.__init__ import storage
+from models.__init__ import models_classes
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+
+
+class HBNBCommand(cmd.Cmd):
+    """Represent a console of HBNBCommand."""
+
+    prompt = "(hbnb) "
+
+    def do_quit(self, arg):
+        """Exit the program."""
+        return (True)
+
+    def do_EOF(self, arg):
+        """Secures a clean exit."""
+        return (True)
+
+    def emptyline(self):
+        """Stop the last executed command"""
+        pass
+
+    def do_create(self, arg):
+        """ Create a new instances of BaseModel
+        class, save it into a JSON file and print id.
+        """
+        args = arg.split()
+        if not arg:
+            print("** class name missing **")
+        elif args[0] not in models_classes:
+            print("** class doesn't exist **")
+        else:
+            new_instance = models_classes[args[0]]()
+            new_instance.save()
+            print(new_instance.id)
+
+    def do_show(self, arg):
+        """ Prints the string representationof an instance
+        based on the class name
+        """
+        args = arg.split()
+        if len(args) == 0:
+            print("** class name missing **")
+        elif args[0] not in models_classes:
+            print("** class doesn't exist **")
+        elif len(args) == 1:
+            print("** instance id missing **")
+        else:
+            key = "{}.{}".format(args[0], args[1])
+            if key in storage.all():
+                print(storage.all()[key])
+            else:
+                print("** no instance found **")
+
+    def do_destroy(self, arg):
+        """ Deletes an instance based on the class
+        name and id; saves the change into de json file
+        """
+        args = arg.split()
+        if len(arg) == 0:
+            print("** class name missing **")
+
+        elif args[0] not in models_classes:
+            print("** class doesn't exist **")
+        elif len(args) == 1:
+            print("** instance id missing **")
+        else:
+            key = args[0] + "." + args[1]
+            instances = storage.all()
+            if key not in instances.keys():
+                print("** no instance found **")
+            else:
+                del instances[key]
+                storage.save()
+
+    def do_all(self, arg):
+        """Prints all string representation of all instances
+        based or not on the class name
+        """
+        obj_list = []
+        cls_k = models_classes.keys()
+        if not arg:
+            for obj in storage.all().values():
+                obj_list.append(str(obj))
+        elif arg in cls_k:
+            for obj in storage.all().values():
+                if isinstance(obj, models_classes[arg]):
+                    obj_list.append(str(obj))
+        else:
+            print("** class doesn't exist **")
+            return
+        print(obj_list)
+
+    def do_update(self, arg):
+        """
+        """
+        args = arg.split()
+        if len(args) == 0:
+            print("** class name missing **")
+        elif args[0] not in models_classes:
+            print("** class doesn't exist **")
+        elif len(args) == 1:
+            print("** instance id missing **")
+        elif len(args) == 3:
+            print("** value missing **")
+        else:
+            key = args[0] + "." + args[1]
+            instances = storage.all()
+            if key not in instances.keys():
+                print("** no instance found **")
+            elif len(args) == 2:
+                print("** attribute name missing **")
+            else:
+                obj = instances[key]
+                setattr(obj, args[2], args[3])
+                storage.save()
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
---
+++ models/base_model.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+# base_model.py
+"""Define a class BaseModel to support other classes.
+"""
+
+import uuid
+from datetime import datetime
+import models
+
+
+class BaseModel:
+    """Represent support to other classes."""
+
+    def __init__(self, *args, **kwargs):
+        """Constructor new instances of class."""
+
+        if kwargs:
+            for key in kwargs:
+                if key != "__class__":
+                    setattr(self, key, kwargs[key])
+
+                if hasattr(self, "created_at") and type(self.created_at) is str:
+                    self.created_at = datetime.strptime(
+                        self.created_at, "%Y-%m-%dT%H:%M:%S.%f"
+                    )
+                if hasattr(self, "updated_at") and type(self.updated_at) is str:
+                    self.updated_at = datetime.strptime(
+                        self.updated_at, "%Y-%m-%dT%H:%M:%S.%f"
+                    )
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            models.storage.new(self)
+
+    def __str__(self):
+        """String representation of the instances."""
+        class_name = type(self).__name__
+        str = "[{}] ({}) {}".format(class_name, self.id, self.__dict__)
+        return str
+
+    def save(self):
+        """Updates the public instance attributes."""
+        self.updated_at = datetime.now()
+        models.storage.save()
+
+    def to_dict(self):
+        """Returns a dictionary containing all keys/values."""
+        n_dict = self.__dict__.copy()
+        if "created_at" in n_dict:
+            n_dict["created_at"] = n_dict["created_at"].isoformat()
+        if "updated_at" in n_dict:
+            n_dict["updated_at"] = n_dict["updated_at"].isoformat()
+        n_dict["__class__"] = self.__class__.__name__
+        return n_dict
