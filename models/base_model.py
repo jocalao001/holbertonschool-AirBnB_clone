81e89dc664b1d703a0fad2132ae19af7131c69e3
jocalao001 <113933963+jocalao001@users.noreply.github.com>
2023-08-05 00:42:18 -0500

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -2,7 +2,6 @@
 # base_model.py
 """Define a class BaseModel to support other classes.
 """
-
 import uuid
 from datetime import datetime
 import models
@@ -23,10 +22,12 @@ class BaseModel:
                     self.created_at = datetime.strptime(
                         self.created_at, "%Y-%m-%dT%H:%M:%S.%f"
                     )
+
                 if hasattr(self, "updated_at") and type(self.updated_at) is str:
                     self.updated_at = datetime.strptime(
                         self.updated_at, "%Y-%m-%dT%H:%M:%S.%f"
                     )
+
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
