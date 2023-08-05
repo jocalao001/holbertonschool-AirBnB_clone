8f9e78dc83c2cf4dee21a67f3ca3f1c270af0e35
jocalao001 <113933963+jocalao001@users.noreply.github.com>
2023-08-05 00:44:28 -0500

    Update file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -15,26 +15,22 @@ from models.review import Review
 
 
 class FileStorage:
-    """Represent a file storage of instances.
-    """
+    """Represent a file storage of instances."""
 
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
-        """Return the dictionary of the class attribute(__objects).
-        """
-        return (self.__class__.__objects)
+        """Return the dictionary of the class attribute(__objects)."""
+        return self.__class__.__objects
 
     def new(self, obj):
-        """Sets in __objects the obj with key (<obj class name>.id).
-        """
+        """Sets in __objects the obj with key (<obj class name>.id)."""
         k_obj = "{}.{}".format(obj.__class__.__name__, obj.id)
         self.__class__.__objects[k_obj] = obj
 
     def save(self):
-        """Serializes __objects to the JSON file(path: __file_path).
-        """
+        """Serializes __objects to the JSON file(path: __file_path)."""
         f_in = FileStorage.__file_path
         d = {}
         for k, v in self.__class__.__objects.items():
